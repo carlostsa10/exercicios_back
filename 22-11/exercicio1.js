ec44e03a0021845f45a44a839c2d63579e746cff
Carlos Tavares <carlostsa10@gmail.com>
2021-11-23 10:50:08 -0300

    Explicação do For..of, While e For de C

---
+++ 22-11/exercicio1.js
@@ -0,0 +1,15 @@
+// FOR... OF
+// Geralmente usado quando eu quero varrer um array inteiro.
+const numeros = [1, 15, 13, 22, 39, 45];
+for (let item of numeros) {
+    console.log(item);
+}
+// Geralmente usa quando não se sabe quantas repetições serão
+while (numeros <= 7){
+    console.log(item);
+    numeros++;
+}
+
+// FOR DE C
+// Geralmente usado quando, além de varrer um array, porém sabendo qual a posição, indice do que esta lendo
+
